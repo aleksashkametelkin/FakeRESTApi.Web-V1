9403bf0c6db65e9c7f5349ae5717c697f8acaa25
Alex Metelkin <aleksashkametelkin@gmail.com>
2023-04-09 16:00:24 +0400

    general improvements

--- tests/test_coverPhotosAPI.py
+++ tests/test_coverPhotosAPI.py
@@ -1,9 +1,13 @@
 import json
+import random
 import main as m
 from utils.models import BaseClass
 
+from faker import Faker
+
 from tests.support.assertions import assert_valid_schema
 
+f = Faker()
 URL = m.TEST_URL
 
 
@@ -34,10 +38,10 @@ def test_get_cover_photo_by_book_id():
     assert response.status_code == 200
 
     j = json.loads(response.content)
-    assert_valid_schema(j, 'cover_photo.json')
+    assert_valid_schema(j, 'get_cover_photo_by_book_id.json')
 
 
-def test_get_cover_photo_id():
+def test_get_cover_photo_by_id():
     # Get Author by Author ID
     # API can respond only user ID from 1 to 10
     response = m.get_cover_photo_by_id()
@@ -48,8 +52,14 @@ def test_get_cover_photo_id():
 
 
 def test_update_cover_photo_by_id(user: BaseClass):
-    # Update User's params
-    payload = m.cover_photo_payload()
+    # Update Cover photo params
+    cover_photo_id = random.randrange(11, 1000)
+    book_id = random.randrange(1, 200)
+    payload = {
+        "id": cover_photo_id,
+        "idBook": book_id,
+        "url": f"{f.url()}"
+    }
     response = m.update_cover_photo(payload)
     assert response.status_code == 200
     cover_photo_id_new = response.json()["id"]
